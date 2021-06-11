8d0bd6e4139442cb30810f6f8ae21b270f55569c
Badr YOUSFI <badr-externe.yousfi@edf.fr>
2021-06-11 16:27:13 +0200

    init

---
+++ app/src/main/java/fr/mappy/fizzbuzz/application/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr.mappy.fizzbuzz.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.mappy.fizzbuzz.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
